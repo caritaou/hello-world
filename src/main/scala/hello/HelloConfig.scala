4f6946bb2dcfb32dfeb603b12ebb053407c0f6a6
Carita Ou <carita.ou@gmail.com>
2015-02-11 15:27:21 -0800

    add RequestMethod.GET

--- src/main/scala/hello/HelloConfig.scala
+++ src/main/scala/hello/HelloConfig.scala
@@ -3,6 +3,7 @@ package hello
 import org.springframework.context.annotation.{Configuration, ComponentScan}
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,7 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @ComponentScan
 @RestController
 class HelloConfig {
-  @RequestMapping(Array("/Hello"))
+//  @RequestMapping(Array("/Hello"))
+  @RequestMapping(value=Array("/"), method = Array(RequestMethod.GET))
+//  @RequestMapping(value=Array("/"), method = Array(RequestMethod.GET, RequestMethod.HEAD))
   def Hello(): String="Hello World!"
 //  def Hello {
 //    println("Hello World")
